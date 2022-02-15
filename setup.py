b08a5ba70c9dc15a69cd14cbd6713e7e26b7eea1
michaelxu01 <michaelxu01@users.noreply.github.com>
2022-02-15 14:23:11 -0500

    fix for m1 pyqt6

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         'matplotlib >= 3.4.2',
         'scikit-image >= 0.17.2',
         'scikit-learn >= 0.23.2',
-        'PyQt5 >= 5.10',
+        'PyQt6 >= 6.2',
         'pyqtgraph >= 0.11',
         'qtconsole >= 4.7.7',
         'ipywidgets >= 7.6.3',
